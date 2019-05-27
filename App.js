dcaa41abb340e946450a4a0c90f909139c26141a
take8 <37664118+take8@users.noreply.github.com>
2019-05-28 03:12:38 +0900

    Retrieve "messages" data

--- App.js
+++ App.js
@@ -6,19 +6,52 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
+const baseUrl = "https://us-central1-frontend-demo-chat.cloudfunctions.net/v1";
+
+type Message = {
+  id: String,
+  body: String,
+  user: {
+    id: string,
+    name: string,
+    avatar: string
+  },
+  date: string
+};
+
+type State = {
+  messages: Array<Message>
+};
+
 type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      messages: []
+    };
+  }
+
+  componentDidMount() {
+    // 初期データのダウンロード
+    fetch(baseUrl + "/channels/general/messages")
+      .then(response => response.json())
+      .then(json => this.setState({ messages: json.messages }))
+      .catch(error => console.log(error));
+  }
+
   render() {
+    console.log(this.state.messages);
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
@@ -32,18 +65,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
