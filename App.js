3d0c6a4f137ce5d9954502709a226f793e2f95a8
take8 <37664118+take8@users.noreply.github.com>
2019-06-01 13:30:46 +0900

    FlatListによるメッセージ一覧の表示

--- App.js
+++ App.js
@@ -7,14 +7,7 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import { Platform, StyleSheet, Text, View, FlatList } from "react-native";
 
 const baseUrl = "https://us-central1-frontend-demo-chat.cloudfunctions.net/v1";
 
@@ -54,9 +47,13 @@ export default class App extends Component<Props, State> {
     console.log(this.state.messages);
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {/* keyExtractor: dataを区別するためのidを取得する関数を設定する */}
+        <FlatList
+          style={styles.list}
+          data={this.state.messages.slice().reverse()}
+          keyExtractor={(item, index) => item.id}
+          renderItem={({ item }) => <Text>{item.body}</Text>}
+        />
       </View>
     );
   }
@@ -69,14 +66,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#F5FCFF"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+  list: {
+    flex: 1
   }
 });
